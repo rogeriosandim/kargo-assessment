2acf9641d87dc25d4ddb7be854aff8e4aece3c0f
Rogerio Sandim <rogeriosandim@Rogerios-MacBook-Pro-2.local>
2024-02-10 22:28:35 +0000

    style: create theme and global scss

---
+++ web/src/theme/index.jsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000'
+    },
+    secondary: {
+      main: '#0077FF'
+    }
+  },
+});
+
+export default theme;
