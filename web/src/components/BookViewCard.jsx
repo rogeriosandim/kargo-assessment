5fe543a5b9559f4e6c5bcc4f8abbec5824d2af13
Rogerio Sandim <rogeriosandim@Rogerios-MacBook-Pro-2.local>
2024-02-10 22:32:36 +0000

    feat: create book card component

---
+++ web/src/components/BookViewCard.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  IconButton,
+  Typography,
+  Collapse
+} from '@mui/material';
+import {
+  Favorite as FavoriteIcon,
+  ExpandMore as ExpandMoreIcon,
+} from '@mui/icons-material';
+import styles from './styles.module.scss';
+import parse from 'html-react-parser';
+import clsx from 'clsx';
+
+  
+const BookViewCard = ({ book }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [favourite, setFavourite] = useState(false);
+  const parsedDescription = parse(`${book.volumeInfo.description}`);
+
+    const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleHeartClick = () => {
+    setFavourite(!favourite);
+  };
+
+   return (
+    <Card className={styles.card}>
+      <CardHeader
+        className={styles.header}
+        title={book.volumeInfo.title}
+        subheader={book.volumeInfo.authors}
+      />
+      <CardMedia
+         id='image'
+         className={styles.media}
+         image={`https://books.google.com/books/publisher/content/images/frontcover/${book.id}?fife=w325-h355&source=gbs_api`}
+      />
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(styles.favourite, {
+            [styles.favouriteSet]: favourite,
+          })}
+          onClick={handleHeartClick}
+          aria-label='add to favourite'
+        >
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(styles.expand, {
+            [styles.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label='show more'
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
+        <CardContent>
+          <Typography variant='body2' color='textSecondary' component='div'>
+            {parsedDescription}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
+export default BookViewCard;
