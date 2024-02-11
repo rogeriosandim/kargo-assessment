206365a3887e67fded8dfaca5fde9ce549be98fe
Rogerio Sandim <rogeriosandim@Rogerios-MacBook-Pro-2.local>
2024-02-11 12:27:47 +0000

    style: update theme colous

--- web/src/theme/index.jsx
+++ web/src/theme/index.jsx
@@ -3,11 +3,23 @@ import { createTheme } from '@mui/material';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#000000'
+      main: '#000000',
     },
     secondary: {
-      main: '#0077FF'
-    }
+      main: '#0077FF',
+    },
+    success: {
+      main: '#00FF77',
+    },
+    info: {
+      main: '#0077FF',
+    },
+    error: {
+      main: '#FF0707',
+    },
+    warning: {
+      main: '#FF7007',
+    },
   },
 });
 
