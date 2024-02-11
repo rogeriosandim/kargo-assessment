f11dc7ca2019d6473458c4730b83cbe467210f10
Rogerio Sandim <rogeriosandim@Rogerios-MacBook-Pro-2.local>
2024-02-11 23:39:14 +0000

    feat: update MyStats page

---
+++ web/src/components/dashboardCard/DashboardCard.jsx
@@ -0,0 +1,26 @@
+// DashboardInfo.js
+import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { alpha } from '@mui/material/styles';
+
+const DashboardCard = ({ title, total, books = false }) => {
+  const statusColorMap = {
+    'In Progress': alpha('#0077FF', 0.45),
+    Finished: alpha('#00FF77', 0.45),
+    Unread: alpha('#000000', 0.45),
+  };
+
+  return (
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent sx={{ backgroundColor: statusColorMap[title] }}>
+          <Typography variant='h6' gutterBottom>
+            {books ? `Books ${title}` : `Pages ${title}`}
+          </Typography>
+          <Typography variant='h4'>{total}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default DashboardCard;
--- web/src/pages/MyStats.jsx
+++ web/src/pages/MyStats.jsx
@@ -1,7 +1,64 @@
-import { Typography } from '@mui/material';
+import DashboardCard from '../components/dashboardCard/DashboardCard';
+import { useBook } from '../contexts/books';
+import { Box, Typography, Grid } from '@mui/material';
 
-const MyStats = () => {
-  return <Typography>MyStats</Typography>;
+const MyStatus = () => {
+  const { readingList } = useBook();
+
+  const countBooksByStatus = (list) => {
+    return list.reduce(
+      (count, book) => {
+        count[book.status] += 1;
+        return count;
+      },
+      { Finished: 0, 'In Progress': 0, Unread: 0 }
+    );
+  };
+
+  const bookStatusCount = countBooksByStatus(readingList);
+
+  const calculatePageCountByStatus = (books) => {
+    const allStatuses = ['Finished', 'In Progress', 'Unread'];
+
+    const pageCount = allStatuses.map((status) => {
+      const pages = books.reduce((sum, book) => {
+        return (
+          sum + (book.status === status ? book.volumeInfo.pageCount || 0 : 0)
+        );
+      }, 0);
+
+      return { pages, status };
+    });
+
+    return pageCount;
+  };
+
+  const pageCountByStatus = calculatePageCountByStatus(readingList);
+
+  return (
+    <Box>
+      <Typography variant='h4' gutterBottom>
+        My Status
+      </Typography>
+      <Grid container spacing={3}>
+        {Object.keys(bookStatusCount).map((count) => (
+          <DashboardCard
+            key={count}
+            title={count}
+            total={bookStatusCount[count]}
+            books={true}
+          />
+        ))}
+        {pageCountByStatus.map((result, index) => (
+          <DashboardCard
+            key={index}
+            title={result.status}
+            total={result.pages}
+          />
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
-export default MyStats;
+export default MyStatus;
