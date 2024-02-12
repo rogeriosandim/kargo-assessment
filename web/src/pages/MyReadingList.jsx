cb63002202abcbdabc811cb2023e8f38a6e436a3
Rogerio Sandim <rogeriosandim@Rogerios-MacBook-Pro-2.local>
2024-02-12 00:55:35 +0000

    feat: add default component when reading list empty

--- web/src/pages/MyReadingList.jsx
+++ web/src/pages/MyReadingList.jsx
@@ -1,6 +1,25 @@
 import { useBook } from '../contexts/books';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import BookViewCard from '../components/bookViewCard/BookViewCard';
+import { Link } from 'react-router-dom';
+
+const NoBookingListMessage = () => {
+  return (
+    <Box textAlign='center' mt={5}>
+      <Typography variant='h5' gutterBottom>
+        No books in your list at the moment :(
+      </Typography>
+      <Typography variant='body1' paragraph>
+        Go search for books and add them to your reading list.
+      </Typography>
+      <Link to='/search-book'>
+        <Button variant='contained' color='primary'>
+          Go to Search
+        </Button>
+      </Link>
+    </Box>
+  );
+};
 
 const MyReadingList = () => {
   const { readingList } = useBook();
@@ -10,18 +29,22 @@ const MyReadingList = () => {
       <Typography variant='h4' gutterBottom>
         My Reading List
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justify='center' spacing={2}>
-            {readingList &&
-              readingList.map((book) => (
-                <Grid key={book.id} item>
-                  <BookViewCard book={book} search={false} />
-                </Grid>
-              ))}
+      {readingList.length ? (
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify='center' spacing={2}>
+              {readingList &&
+                readingList.map((book) => (
+                  <Grid key={book.id} item>
+                    <BookViewCard book={book} search={false} />
+                  </Grid>
+                ))}
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      ) : (
+        <NoBookingListMessage />
+      )}
     </Box>
   );
 };
